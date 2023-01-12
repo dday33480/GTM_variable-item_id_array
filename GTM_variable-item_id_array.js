0d37b5667769268a27b739b0d1e3bb490309bfbb
dday33480 <daviddufau@hotmail.co.uk>
2023-01-12 16:54:23 +0100

    Added comments to code

--- GTM_variable-item_id_array.js
+++ GTM_variable-item_id_array.js
@@ -1,8 +1,12 @@
 function () { 
+
+  //Get items object from dataLayer
    var items = google_tag_manager['GTM-XXXXXXX'].dataLayer.get('ecommerce').items;
   
+   //Empty array to store itemsIds
   var itemIds = [];
-  
+
+  //Iterate over each item in the dataLayer object to get each item_id
   for(i=0; i<items.length; i++) {
   itemIds.push(items[i].item_id);
   } 
